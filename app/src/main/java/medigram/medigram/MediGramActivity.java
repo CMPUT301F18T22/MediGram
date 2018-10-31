54311e0e79968907057d6ed6676398bdd381e6eb
amohamed <amohamed@ualberta.ca>
2018-10-31 12:48:37 -0600

    Anas - unitTest branch initiated along with bare Android Project
    Created a unitTest branch. Added a barebone android project to work on.

---
+++ app/src/main/java/medigram/medigram/MediGramActivity.java
@@ -0,0 +1,13 @@
+package medigram.medigram;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MediGramActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medi_gram);
+    }
+}

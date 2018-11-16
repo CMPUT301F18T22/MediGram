51fb36a4a26ac78c55fb9c748544a40c9bc25794
jiaqi11 <jiaqi11@ualberta.ca>
2018-11-16 10:25:59 -0700

    half finished take a photo
    so this is where we stuck on, i am tring to get the camera going in class take_a_photo, but i am facing  an error with file provider, all the coding for camera is from https://developer.android.com/guide/topics/media/camera, but some how it is not working on my computer, even if i install the support tool.  Besides, we are also confused on how to link the record part to other parts, if someone can tell us how to update the right way  it could be helpful thx

--- app/src/main/java/medigram/medigram/take_a_photo.java
+++ app/src/main/java/medigram/medigram/take_a_photo.java
@@ -1,35 +1,59 @@
 package medigram.medigram;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class take_a_photo extends Activity {
-    public static final int TAKE_PHOTO = 1;
-    public static final int CROP_PHOTO = 2;
-    private Button takePhoto;
-    //private ImageView picture;
-    private Uri imageUri;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-            @Override
-            public void onClick(View v) {
-                File outputImage = new File(Environment.
-                        getExternalStorageDirectory(), "tempImage.jpg");
-                try {
-                    if (outputImage.exists()) {
-                        outputImage.delete();
-                    }
-                    outputImage.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                imageUri = Uri.fromFile(outputImage);
-                Intent intent = new Intent("android.media.action. IMAGE_CAPTURE");
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-                startActivityForResult(intent, TAKE_PHOTO); // 启动相机程序
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private String mCurrentPhotoPath;
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Toast.makeText(this, "picuture error", Toast.LENGTH_SHORT).show();
+                return;
             }
-        });
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, 1);
+            }
+        }
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
     }

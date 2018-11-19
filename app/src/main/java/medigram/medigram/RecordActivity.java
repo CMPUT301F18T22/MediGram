a6189692a71cc186693230b03ca07f9b2c5f65e1
Yuhang Xie <jeremy@Yuhangs-MBP.hitronhub.home>
2018-11-18 18:19:09 -0700

    addComment dialog finished,cannot store data

--- app/src/main/java/medigram/medigram/RecordActivity.java
+++ app/src/main/java/medigram/medigram/RecordActivity.java
@@ -1,13 +1,83 @@
 package medigram.medigram;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RecordActivity extends AppCompatActivity {
+    private Patient patient;
+    private Button addCommentBTN;
+    private EditText editText;
+/*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            if (requestCode == 1) {
+                patient = (Patient) data.getSerializableExtra("newPatient");
+                patients.addPatient(patient); // add the patient
+                accountManager.careProviderUpdater(careProvider.getUserID(), careProvider);
 
+                String userID = patient.getUserID();
+                int numOfProblems = patient.getNumOfProblems();
+                searchInfo = new PatientSearchInfo(userID, numOfProblems);
+                searchInfos.add(searchInfo);
+
+                searchAdapter = new SearchPatientAdapter(this, searchInfos);
+                listViewPatients.setAdapter(searchAdapter);
+            }
+        }
+    }
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        patient = (Patient) getIntent().getSerializableExtra("Patient");
+
+
+        addCommentBTN = findViewById(R.id.addComment);
+        addCommentBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(RecordActivity.this);
+                View view = getLayoutInflater().inflate(R.layout.dialog_add_comment, null);
+                editText = (EditText) view.findViewById(R.id.enterComment);
+                Button confirmbtn = (Button) view.findViewById(R.id.confirm);
+                confirmbtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!editText.getText().toString().isEmpty()){
+                            Toast.makeText(RecordActivity.this,
+                                    R.string.success_addComment_msg,
+                                    Toast.LENGTH_SHORT).show();
+
+                        }else{
+                            Toast.makeText(RecordActivity.this,
+                                    R.string.no_comment_msg,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        Intent intent = new Intent(RecordActivity.this, RecordActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                builder.setView(view);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                //Intent intent = new Intent(RecordActivity.this, RecordActivity.class);
+                //intent.putExtra("Patient", patient);
+                //startActivityForResult(intent,1);
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/medigram/medigram/RecordListActivity.java
+++ app/src/main/java/medigram/medigram/RecordListActivity.java
@@ -1,19 +1,27 @@
 package medigram.medigram;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Filterable;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
-
-public class RecordListActivity  extends AppCompatActivity {     private ListView recordsView;
-    private ArrayAdapter<Record> adapter;
+public class RecordListActivity  extends AppCompatActivity {
+    //private ListView recordsView;
+    private ArrayAdapter<String> adapter;
     private ArrayList<Record> recordList;
     private AccountManager accountManager;
     private Patient patient;
@@ -23,6 +31,7 @@ public class RecordListActivity  extends AppCompatActivity {     private ListVie
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String items[] = new String[] {"apple", "pen"};
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_list);
         accountManager = new AccountManager(getApplicationContext());
@@ -38,13 +47,25 @@ public class RecordListActivity  extends AppCompatActivity {     private ListVie
         TextView Displaydate = findViewById(R.id.timestamp);
         TextView Displaydes = findViewById(R.id.Description);
 
-
         Displayproblemtile.setText(problem_tile);
         Displaydate.setText(date);
         Displaydes.setText(description);
-        recordsView = (ListView) findViewById(R.id.RecordListView);
-        recordsView.setTextFilterEnabled(true);
+
+        Button button = findViewById(R.id.addrecordbtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecordListActivity.this, RecordActivity.class);
+                intent.putExtra("Patient", patient);
+                startActivity(intent);
+            }
+        });
+        // for testing only, need to be deleted
+        //ListView listView = (ListView) findViewById(R.id.RecordListView);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_record,items);
+        //listView.setAdapter(adapter);
+
     }
-    }
+}
 
 

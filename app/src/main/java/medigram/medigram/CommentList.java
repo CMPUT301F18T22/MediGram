dbde2caa69eb15ccfc8a7f500814dafe369e5765
jeremy763 <xyh1997jeremy@gmail.com>
2018-12-03 13:53:34 -0700

    javadoc for commentlist updated

--- app/src/main/java/medigram/medigram/CommentList.java
+++ app/src/main/java/medigram/medigram/CommentList.java
@@ -10,29 +10,29 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Contains a List of Problems, with methods that modify or return problems
+ * Contains a List of comments, with methods that modify or return comments
  * @author Jeremy Xie
  */
 public class CommentList implements Serializable{
     private ArrayList<Comment> commentList = new ArrayList<Comment>();
 
     /**
-     * Adds a problem to the problem list
-     * @param comment The problem to be added
+     * Adds a comment to the comment list
+     * @param comment The comment to be added
      */
     public void addComment(Comment comment){
         commentList.add(comment);
     }
 
     /**
-     * Gets the problem list
-     * @return The list of problems
+     * Gets the comment list
+     * @return The list of comments
      */
     public ArrayList<Comment> getList() {return commentList;}
 
     /**
-     * Removes a problem at the given index
-     * @param index The int index of the problem to be removed
+     * Removes a comment at the given index
+     * @param index The int index of the comment to be removed
      */
     public void removeIndex(int index){commentList.remove(index);
     }
@@ -41,26 +41,26 @@ public class CommentList implements Serializable{
     }
 
     /**
-     * Gets the problem at the given index
-     * @param index The index of the problem to be returned
-     * @return The problem at a specified index
+     * Gets the comment at the given index
+     * @param index The index of the comment to be returned
+     * @return The comment at a specified index
      */
     public void updateComment(int index, Comment comment){
         commentList.set(index, comment);
     }
 
     /**
-     * Gets the problem at the given index
-     * @param index The index of the problem to be returned
-     * @return The problem at a specified index
+     * Gets the comment at the given index
+     * @param index The index of the comment to be returned
+     * @return The comment at a specified index
      */
     public Comment getComment(int index){return commentList.get(index);
     }
 
     /**
-     * Gets the index of the given problem
-     * @param comment The given problem
-     * @return The int index of the problem that matches the given problem
+     * Gets the index of the given comment
+     * @param comment The given comment
+     * @return The int index of the comment that matches the given comment
      */
     public int getIndex(Comment comment){
         return commentList.indexOf(comment);
@@ -71,8 +71,8 @@ public class CommentList implements Serializable{
     }
 
     /**
-     * Gets the size of the problem list
-     * @return Int size of the problem list
+     * Gets the size of the comment list
+     * @return Int size of the comment list
      */
     public int getSize(){
         return commentList.size();
